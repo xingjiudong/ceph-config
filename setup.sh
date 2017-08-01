4d890321e2787cd8d085700054c146bdc68358a4
xingjiudong <25635680@qq.com>
2017-08-01 08:07:23 +0800

    add env

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 sed -r "s/@CLUSTER@/${CLUSTER:-ceph}/g" /etc/confd/conf.d/ceph.conf.toml.in > /etc/confd/conf.d/ceph.conf.toml
 confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP:-127.0.0.1}:2379 --prefix="/ceph-config/${CLUSTER:-ceph}" > /dev/null 2>&1
 
-etcdctl  --peers http://172.23.7.63:2379 get  /ceph-config/ceph/adminKeyring  > /etc/ceph/ceph.client.admin.keyring
+etcdctl  --peers http://${ETCD_CLIENT_IP:-127.0.0.1}:2379 get  /ceph-config/${CLUSTER:-ceph}/adminKeyring  > /etc/ceph/${CLUSTER:-ceph}.client.admin.keyring
 
-cp /etc/ceph/ceph.conf /tmp/ceph/ceph.conf
-cp /etc/ceph/ceph.client.admin.keyring /tmp/ceph/ceph.client.admin.keyring
+cp /etc/ceph/${CLUSTER:-ceph}.conf  /tmp/ceph/${CLUSTER:-ceph}.conf
+cp /etc/ceph/${CLUSTER:-ceph}.client.admin.keyring /tmp/ceph/${CLUSTER:-ceph}.client.admin.keyring
