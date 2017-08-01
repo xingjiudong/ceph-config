4c9250e70c2feb7c90bb501c071a0c84b1e8e439
xingjiudong <25635680@qq.com>
2017-08-01 18:29:41 +0800

    update shell

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-sed -r "s/@CLUSTER@/${CLUSTER:-ceph}/g" /etc/confd/conf.d/ceph.conf.toml.in > /etc/confd/conf.d/ceph.conf.toml
-confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP:-127.0.0.1}:2379 --prefix="/ceph-config/${CLUSTER:-ceph}" > /dev/null 2>&1
+: ${CLUSTER:=ceph}
+: ${ETCD_CLIENT_IP:=127.0.0.1}
 
-etcdctl  --peers http://${ETCD_CLIENT_IP:-127.0.0.1}:2379 get  /ceph-config/${CLUSTER:-ceph}/adminKeyring  > /etc/ceph/${CLUSTER:-ceph}.client.admin.keyring
+sed -r "s/@CLUSTER@/${CLUSTER}/g" /etc/confd/conf.d/ceph.conf.toml.in > /etc/confd/conf.d/ceph.conf.toml
+confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/ceph-config/${CLUSTER}"
 
-cp /etc/ceph/${CLUSTER:-ceph}.conf  /tmp/ceph/${CLUSTER:-ceph}.conf
-cp /etc/ceph/${CLUSTER:-ceph}.client.admin.keyring /tmp/ceph/${CLUSTER:-ceph}.client.admin.keyring
+etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get  /ceph-config/${CLUSTER}/adminKeyring  > /etc/ceph/${CLUSTER}.client.admin.keyring
