7824a10c31e93740e5f11affb54fee16b491dc70
xingjiudong <25635680@qq.com>
2017-08-02 15:45:53 +0800

    fix admin.keyring permission

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 set -e
 
 : ${CLUSTER:=ceph}
-: ${ETCD_CLIENT_IP:=127.0.0.1}
+: ${ETCD_CLIENT_IP}
 
 sed -r "s/@CLUSTER@/${CLUSTER}/g" /etc/confd/conf.d/ceph.conf.toml.in > /etc/confd/conf.d/ceph.conf.toml
 confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/ceph-config/${CLUSTER}"
 
 etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get  /ceph-config/${CLUSTER}/adminKeyring  > /etc/ceph/${CLUSTER}.client.admin.keyring
+chmod 600 /etc/ceph/${CLUSTER}.client.admin.keyring
